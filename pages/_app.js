0e815b412e9440a5bcf8bd59885a4c7d1350d3a5
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-25 04:34:35 -0500

    Complete refractoring MyApp.js into a functional component

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import App from 'next/app';
 import axios from 'axios';
 import { parseCookies, destroyCookie } from 'nookies';
 import baseUrl from '../utils/baseUrl';
@@ -6,9 +5,17 @@ import { redirectUser } from '../utils/authUser';
 import Layout from '../components/Layout/Layout';
 import 'semantic-ui-css/semantic.min.css';
 
-class MyApp extends App {
 
-	static async getInitialProps({ Component, ctx }) {
+function MyApp({ Component, pageProps }) {
+
+	return (
+		<Layout {...pageProps}>
+			<Component {...pageProps} />
+		</Layout>
+	);
+}
+
+MyApp.getInitialProps = async ({ Component, ctx }) => {
 		const { token } = parseCookies(ctx);
 		let pageProps = {};
 
@@ -32,26 +39,12 @@ class MyApp extends App {
 
 				pageProps.user = user;
 				pageProps.userFollowStats = userFollowStats;
-				
 			} catch (error) {
 				destroyCookie(ctx, 'token');
 				redirectUser(ctx, '/login');
-
 			}
 		}
 
 		return { pageProps };
-	}
-
-	render() {
-		const { Component, pageProps } = this.props;
-
-		return (
-			<Layout {...pageProps}>
-				<Component {...pageProps} />
-			</Layout>
-		);
-	}
 }
-
 export default MyApp;
