af3ba935330cf915b523bb486eacfe89b530df9c
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-25 04:22:24 -0500

    Complete setup of basic authentication

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import baseUrl from '../utils/baseUrl';
 
-export default function Index() {
+export default function Index({ user, userFollowStats }) {
+	console.log({user, userFollowStats })
 	return (
 		<div>
 			<h2>this is homepage</h2>
