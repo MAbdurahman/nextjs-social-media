deb58fde53cd9cadd84ff781579f84d33c0a03b4
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-27 03:56:40 -0500

    Complete hotfix bug in SearchComponent.js handleChange method

--- api/search.js
+++ api/search.js
@@ -6,16 +6,19 @@ const UserModel = require('../models/UserModel');
 router.get('/:searchText', authMiddleware, async (req, res) => {
 	try {
 		const { searchText } = req.params;
+	
 
 		if (searchText.length === 0) {
 			return;
 		}
+	
 
 		const results = await UserModel.find({
 			name: { $regex: searchText, $options: 'i' },
 		});
 
 		return res.status(200).json(results);
+
 	} catch (error) {
 		console.error(error);
 		return res.status(500).send(`Server Error`);
