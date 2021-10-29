1beb7f5802f0733e2619315e88efb72896409851
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-29 11:06:58 -0500

    Implement infinite scroll for index.js

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,19 @@ import { Segment } from 'semantic-ui-react';
 import { parseCookies } from 'nookies';
 import { NoPosts } from '../components/Layout/NoData';
 import { PostDeleteToastr } from '../components/Layout/Toastr';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import {
+	PlaceHolderPosts,
+	EndMessage,
+} from '../components/Layout/PlaceHolderGroup';
+import cookie from 'js-cookie';
 
 export default function Index({ user, postsData, errorLoading }) {
 	//**************** variables ****************//
 	const [posts, setPosts] = useState(postsData || []);
 	const [showToastr, setShowToastr] = useState(false);
+	const [hasMore, setHasMore] = useState(true);
+	const [pageNumber, setPageNumber] = useState(2);
 	//**************** functions ****************//
 	useEffect(() => {
 		document.title = `Welcome, ${user.name.split(' ')[0]}`;
@@ -21,6 +29,22 @@ export default function Index({ user, postsData, errorLoading }) {
 		showToastr && setTimeout(() => setShowToastr(false), 3000);
 	}, [showToastr]);
 
+	const fetchDataOnScroll = async () => {
+		try {
+			const res = await axios.get(`${baseUrl}/api/posts`, {
+				headers: { Authorization: cookie.get('token') },
+				params: { pageNumber },
+			});
+
+			if (res.data.length === 0) setHasMore(false);
+
+			setPosts(prev => [...prev, ...res.data]);
+			setPageNumber(prev => prev + 1);
+		} catch (error) {
+			alert('Error fetching Posts');
+		}
+	};
+
 	if (posts.length === 0 || errorLoading) return <NoPosts />;
 
 	return (
@@ -28,16 +52,23 @@ export default function Index({ user, postsData, errorLoading }) {
 			{showToastr && <PostDeleteToastr />}
 			<Segment>
 				<CreatePost user={user} setPosts={setPosts} />
-
-				{posts.map(post => (
-					<CardPost
-						key={post._id}
-						post={post}
-						user={user}
-						setPosts={setPosts}
-						setShowToastr={setShowToastr}
-					/>
-				))}
+				<InfiniteScroll
+					hasMore={hasMore}
+					next={fetchDataOnScroll}
+					loader={<PlaceHolderPosts />}
+					endMessage={<EndMessage />}
+					dataLength={posts.length}
+				>
+					{posts.map(post => (
+						<CardPost
+							key={post._id}
+							post={post}
+							user={user}
+							setPosts={setPosts}
+							setShowToastr={setShowToastr}
+						/>
+					))}
+				</InfiniteScroll>
 			</Segment>
 		</>
 	);
