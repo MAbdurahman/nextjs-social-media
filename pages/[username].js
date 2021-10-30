013ed1bfe6819405524fd556b5d6e342ed2ff00a
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-30 04:57:51 -0500

    Add functionality to bring user data from backend

--- pages/[username].js
+++ pages/[username].js
@@ -3,16 +3,56 @@ import { useRouter } from 'next/router';
 import axios from 'axios';
 import baseUrl from '../utils/baseUrl';
 import { parseCookies } from 'nookies';
+import cookie from 'js-cookie';
+import { NoProfile } from './../components/Layout/NoData';
 
-export default function ProfilePage() {
+export default function ProfilePage({
+	errorLoading,
+	profile,
+	followersLength,
+	followingLength,
+	user,
+	userFollowStats,
+}) {
 	//**************** variables ****************//
-   const router = useRouter();
-   const { username } = router.query;
+	const router = useRouter();
+	const [posts, setPosts] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	//**************** functions ****************//
+	if (errorLoading) return <NoProfile />;
+
+	useEffect(() => {
+		const getPosts = async () => {
+			setLoading(true);
+
+			try {
+				const { username } = router.query;
+				const res = await axios.get(
+					`${baseUrl}/api/profile/posts/${username}`,
+					{
+						headers: { Authorization: cookie.get('token') },
+					}
+				);
+
+				setPosts(res.data);
+
+			} catch (error) {
+				alert('Error Loading Posts');
+			}
+
+			setLoading(false);
+		};
+
+		getPosts();
+      
+	}, [router.query.username]);
+
 	return (
-      <div>
-         <h2>{username}</h2>
-      </div>
-   )
+		<div>
+			<h2>profile page</h2>
+		</div>
+	);
 }
 
 ProfilePage.getInitialProps = async ctx => {
@@ -27,9 +67,7 @@ ProfilePage.getInitialProps = async ctx => {
 		const { profile, followersLength, followingLength } = res.data;
 
 		return { profile, followersLength, followingLength };
-
 	} catch (error) {
 		return { errorLoading: true };
-      
 	}
 };
